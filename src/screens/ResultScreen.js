0e64585e2b5e05404975e5793869be446300195e
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-03-09 02:11:42 -0500

    Built out Single Result Screen with retreived Images

--- src/screens/ResultScreen.js
+++ src/screens/ResultScreen.js
@@ -1,16 +1,50 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, Text, StyleSheet, FlatList } from 'react-native';
+
+// API
+import yelp from '../api/yelp';
 
 const ResultScreen = ({ route }) => {
     const { id } = route.params;
+    const [ result, setResult ] = useState(null);
+
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+    };
+
+    useEffect(() => {
+        getResult(id);
+    }, []);
+
+    if (!result) {
+        return null;
+    }
 
     return (
         <View>
-            <Text>{id}</Text>
+            <Text>{result.name}</Text>
+            <FlatList 
+                data={result.photos}
+                keyExtractor={photo => photo}
+                renderItem={({ item }) => {
+                    return (
+                        <Image 
+                            style={styles.image} 
+                            source={{uri: item}} 
+                        />
+                    )
+                }}
+            />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        height: 160,
+        width: 250
+    }
+});
 
 export default ResultScreen;

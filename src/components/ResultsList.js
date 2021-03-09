513c62d6a48b1d57e49c21c9d535580bb5338608
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-03-09 02:17:00 -0500

    Added functionality for removing empty listings

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -8,6 +8,10 @@ import ResultsDetail from './ResultsDetail';
 const ResultsList = ({ title, results }) => {
     const navigation = useNavigation();
 
+    if (!results.length) {
+        return null
+    }
+
     return (
         <View style={styles.container}>
             <Text 
